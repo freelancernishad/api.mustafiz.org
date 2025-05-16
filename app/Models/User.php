8a6ca85c8d92858b240032dc6df2634052b88f91
Nishad Hossain <freelancernishad123@gmail.com>
2025-05-16 22:53:10 +0600

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -105,7 +105,7 @@ class User extends Authenticatable implements JWTSubject
 
     public function getDobAttribute($value)
     {
-        return \Carbon\Carbon::parse($value)->format('Y-m-d');
+        return \Carbon\Carbon::parse($value)->format('m-d-Y');
     }
 
 
